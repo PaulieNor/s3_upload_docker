979499798ce0f40ccd41a5dd74838b12588b7ce1
Paulie <paul.91.norman@gmail.com>
2024-04-07 20:26:14 +0100

    TFM

--- tfm/upload/ssm/parameters.tf
+++ tfm/upload/ssm/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "s3_upload_bucket_name_store" {
-  name  = "s3_upload_bucket_name"
+  name  = "upload-bucket-name"
   type  = "String"
   value = var.s3_upload_bucket_name
 }
